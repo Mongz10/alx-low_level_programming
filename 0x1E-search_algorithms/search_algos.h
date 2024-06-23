addf7835cb0976ec13a933419dd3c35bae6e5ead
Mongz10 <m.t.sibande@gmail.com>
2024-06-23 12:08:08 -0700

    0x1E-search_algorithms

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,9 +1,9 @@
-#ifndef SEARCH_H
-#define SEARCH_H
+#ifndef _SORTING_ALGOS_H_
+#define _SORTING_ALGOS_H_
 
-#include <stdlib.h>
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct listint_s - singly linked list
@@ -13,36 +13,39 @@
  * @next: Pointer to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
  */
 typedef struct listint_s
 {
-	int n;
-	size_t index;
-	struct listint_s *next;
+    int n;
+    size_t index;
+    struct listint_s *next;
 } listint_t;
 
-
-/*
- * Function that searches for a value in an
- * array of integers using the Linear search algorithm
+/**
+ * struct skiplist_s - Singly linked list with an express lane
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
+ *
+ * Description: singly linked list node structure with an express lane
  */
+typedef struct skiplist_s
+{
+    int n;
+    size_t index;
+    struct skiplist_s *next;
+    struct skiplist_s *express;
+} skiplist_t;
+
 int linear_search(int *array, size_t size, int value);
-
-/*
- * Function that searches for a value in a sorted array
- * of integers using the Binary search algorithm
- */
 int binary_search(int *array, size_t size, int value);
-
-/*
- * Function that searches for a value in a
- * sorted array of integers using the Jump search algorithm
- */
 int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
 
-/*
- * Function that searches for a value in a sorted array of
- * integers using the Interpolation search algorithm
- */
-
+#endif
